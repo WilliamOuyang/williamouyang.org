028b6f7ec83af0e8adcc8e62a487a29892e4fd1e
William Ouyang <gwko624p@gmail.com>
2024-06-07 01:17:54 -0700

    remove unused

--- src/pagecomponents/shared/NavResources.tsx
+++ src/pagecomponents/shared/NavResources.tsx
@@ -1,43 +1,24 @@
-import Image from "@/assets/react.svg"
 import { Card } from "@/components/ui/card"
 
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 
-export interface Artwork {
-  artist: string
-  art: string
-}
 
-export const works: Artwork[] = [
-  {
-    artist: "Ornella Binni",
-    art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
-  },
-  {
-    artist: "Tom Byrom",
-    art: "https://images.unsplash.com/photo-1548516173-3cabfa4607e9?auto=format&fit=crop&w=300&q=80",
-  },
-  {
-    artist: "Vladimir Malyavko",
-    art: "https://images.unsplash.com/photo-1494337480532-3725c85fd2ab?auto=format&fit=crop&w=300&q=80",
-  },
-]
 
 const NavResources=()=> {
   return (
     <ScrollArea className="w-full whitespace-nowrap rounded-lg border bg-card" style={{height:'300px'}}>
       <div style={{display:'flex',padding:'15px', gap:'15px'}}>
        <Card style={{height:'260px', width:'33vw'}}>
-        One
+        Schedule
        </Card>
        <Card style={{height:'260px', width:'33vw'}}>
-        Two
+        Dashboard
        </Card>
        <Card style={{height:'260px', width:'33vw'}}>
-        Three
+        Personal Vault
        </Card>
        <Card style={{height:'260px', width:'33vw'}}>
-        Four
+        News
        </Card>
        <Card style={{height:'260px', width:'33vw'}}>
         Five
@@ -52,7 +33,7 @@ const NavResources=()=> {
         Eight
        </Card>
        <Card style={{height:'260px', width:'33vw'}}>
-        Nine
+        White paper
        </Card>
 
       </div>
