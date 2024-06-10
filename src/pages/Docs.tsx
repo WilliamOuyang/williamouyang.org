7e2ece326e6f30db6407e831e52ef9a2fd9d6143
William Ouyang <gwko624p@gmail.com>
2024-06-10 09:31:02 -0700

    added doc

--- src/pages/Docs.tsx
+++ src/pages/Docs.tsx
@@ -5,8 +5,12 @@ const Docs = () => {
     <div>
         
         <Navbar2/>
-        <div style={{marginTop:'100px'}}>
-          Notes:
+        <div style={{marginTop:'100px', padding:'10px'}}>
+          (6/10/2024): <br/> <br/>
+          Begun college planning. My strategy is minimize cost via 2 years of community college (free of charge), then transfer to UC for 2 years.<br/> 
+          Taken on an additional job - teaching chess for $90 an hour. <br/>
+          Currently working at chipotle for $20.17/hour. <br/>
+          Researching which campus on The Real World fits suits my circumstances. 
           
 
         </div>
