095bcd3f8f09ec70bd25f3b1fd59e54c0d1d3f1c
William Ouyang <williamouyang@gmx.com>
2024-06-18 18:13:00 -0700

    supabase connect success

---
+++ src/pages/Test.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+
+const supabase = createClient("https://bsqkowajqcuuiaybhumq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzcWtvd2FqcWN1dWlheWJodW1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg2ODY3MjYsImV4cCI6MjAzNDI2MjcyNn0.JQziKNIcTKGBK0jbF7GZj5a0PawnZY2vHX9-vILJcJw");
+
+function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  async function getCountries() {
+    const { data } = await supabase.from("countries").select();
+    setCountries(data);
+  }
+
+  return (
+    <ul>
+      {countries.map((country) => (
+        <li key={country.name}>{country.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default App;
