eb7edff0d8e0a1d5c82f79a3f41115e0c35ccd83
William Ouyang <williamouyang@gmx.com>
2024-06-23 11:02:03 -0700

    functioning relative absolute navbar

--- src/pagecomponents/shared/Navbar.tsx
+++ src/pagecomponents/shared/Navbar.tsx
@@ -8,38 +8,106 @@ const Navbar = () => {
   
 
   return (
+    <>
+
     <div
-      className="bg-card border drop-shadow-md"
+      className="bg-card border"
+      // drop-shadow-md
       style={{
-        position: "fixed",
-        top: "10px",
+        position: "relative",
+        // top: "10px",
         width: "100%",
         height: "75px",
         alignContent: "center",
       }}
+      
 >
+  
       <div
         style={{
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          height:'100%'
-          
+          height:'100%',
+          gap: '30px'
         }}
-        onMouseEnter={() => {
-            setMenu1(false);
-          }}
+        // onMouseEnter={() => {
+        //     setMenu1(false);
+        //   }}
       >
 
 
+<h1 style={{ position: "absolute", left: "60px", fontFamily: 'GT'}}>
+<a href="/">
+     williamouyang.org
+</a>
+</h1>
+
+<div style={{position:'absolute', right:"60px"}}>
+    <ModeToggle />
+  </div>
 
 
-<h1 style={{ position: "fixed", left: "60px", fontSize:'30px', fontFamily: 'Cloister', /* Use a fallback serif font */}}>
-      <a href="/">  OUYANG</a>
-    </h1>
 
 
-        <a className="mx-4" href="/">Introduction</a>
+
+        
+{/* Introduction Code Below */}
+<div className="menu-parent" style={{ position: "relative"}}>
+
+<div style={{display:'flex', alignContent:'center'}} onMouseEnter={() => setMenu2(true)} onMouseLeave={() => setMenu2(false)}>
+    <h1 className="mr-2">Introduction</h1>
+    <img
+ src={ArrowDown}
+    style={{
+    transform: Menu2 ? "rotate(180deg)" : "rotate(0deg)",
+    cursor: "pointer",
+    }}/>
+</div>
+
+{Menu2 && (
+<div
+style={{
+position: "absolute",
+top: "100%",
+right: 0,
+zIndex: 100,
+minWidth: "220px",
+maxWidth: "220px",
+}}
+onMouseEnter={() => setMenu2(true)} 
+onMouseLeave={() => setMenu2(false)}
+className="bg-card border"
+>
+<ul>
+<a href="/">
+<li className="hover:bg-accent" style={{ padding: "6px 10px 3px 15px"}}>
+  Home
+</li>
+</a>
+<a href="/vault">
+<li className="hover:bg-accent" style={{ padding: "6px 10px 3px 15px" }}>
+About
+</li>
+</a>
+<a href="/storage">
+<li className="hover:bg-accent" style={{ padding: "6px 10px 3px 15px" }}>
+Contact
+</li>
+</a>
+<a href="/whitepaper">
+<li className="hover:bg-accent" style={{ padding: "6px 10px 3px 15px" }}>
+White paper
+</li>
+</a>
+</ul>
+</div>
+)}
+
+</div>
+{/* Introduction Code Above */}
+
+
 
 {/* Resources Code Below */}
         <div className="menu-parent" style={{ position: "relative"}}>
@@ -98,16 +166,16 @@ const Navbar = () => {
 
 
 
-        <a className="mx-4" href="/docs">Documentation</a>
+        <a href="/docs">Documentation</a>
 
-        <a className="mx-4" href="faq">FAQ</a>
+        <a className="" href="faq">FAQ</a>
 
-        <a style={{ position: "fixed", right: "60px" }}>
-          <ModeToggle />
-        </a>
+
+
 
       </div>
     </div>
+    </>
   );
 };
 
