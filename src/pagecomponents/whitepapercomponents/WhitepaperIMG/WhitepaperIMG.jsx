9be54c32be98e44a739031c04ff89d5d161d740b
William Ouyang <williamouyang@gmx.com>
2024-06-27 23:29:57 -0700

    change system whitepaper txt to img jpg

---
+++ src/pagecomponents/whitepapercomponents/WhitepaperIMG/WhitepaperIMG.jsx
@@ -0,0 +1,25 @@
+import one from '@/pagecomponents/whitepapercomponents/WhitepaperIMG/The System_page-0001.jpg'
+import two from '@/pagecomponents/whitepapercomponents/WhitepaperIMG/The System_page-0002.jpg'
+import three from '@/pagecomponents/whitepapercomponents/WhitepaperIMG/The System_page-0003.jpg'
+import four from '@/pagecomponents/whitepapercomponents/WhitepaperIMG/The System_page-0004.jpg'
+import five from '@/pagecomponents/whitepapercomponents/WhitepaperIMG/The System_page-0005.jpg'
+import six from '@/pagecomponents/whitepapercomponents/WhitepaperIMG/The System_page-0006.jpg'
+import seven from '@/pagecomponents/whitepapercomponents/WhitepaperIMG/The System_page-0007.jpg'
+import eight from '@/pagecomponents/whitepapercomponents/WhitepaperIMG/The System_page-0008.jpg'
+
+const WhitepaperIMG = () => {
+  return (
+    <>
+        <img src={one}/>
+        <img src={two}/>
+        <img src={three}/>
+        <img src={four}/>
+        <img src={five}/>
+        <img src={six}/>
+        <img src={seven}/>
+        <img src={eight}/>
+    </>
+  )
+}
+
+export default WhitepaperIMG
--- src/pages/Whitepaper.jsx
+++ src/pages/Whitepaper.jsx
@@ -1,9 +1,10 @@
-import WhitepaperText from "@/pagecomponents/whitepapercomponents/WhitepaperText";
+// import WhitepaperText from "@/pagecomponents/whitepapercomponents/WhitepaperText";
 import Download from "@/pagecomponents/whitepapercomponents/Download";
 import { motion, useScroll, useSpring } from "framer-motion";
 import Navbar from "@/pagecomponents/shared/Navbar";
 import { useEffect } from "react";
 import Footer from "@/pagecomponents/shared/Footer";
+import WhitepaperIMG from "@/pagecomponents/whitepapercomponents/WhitepaperIMG/WhitepaperIMG";
 
 const Whitepaper = () => {
   const { scrollYProgress } = useScroll();
@@ -21,11 +22,11 @@ const Whitepaper = () => {
       <Navbar/>
       <>
         <motion.div className="progress-bar" style={{ scaleX }} />
-        <WhitepaperText />  
+        {/* <WhitepaperText />   */}
+        <WhitepaperIMG/>
       </>
       <Download />
       <Footer/>
-      <TheSystem/>
     </>
   );
 };
