80a492dae6ede8429e6963d36216dcf60f1ca110
William Ouyang <williamouyang@gmx.com>
2024-07-01 17:49:43 -0700

    reverted white paper style, removed stylex, added updated white paper

--- src/pagecomponents/whitepapercomponents/Download.tsx
+++ src/pagecomponents/whitepapercomponents/Download.tsx
@@ -8,7 +8,7 @@ function download(){
 const Download = () => {
   return (
     <div style={{display:'flex', width:'100%', justifyContent:'center', marginTop:'50px', marginBottom:'50px'}}>
-        <button style={{display:'flex', alignItems:'center'}} onClick={download}><img src={ico_download} className="mr-2"></img> English (Original)</button>
+        <button style={{display:'flex', alignItems:'center'}} onClick={download}><img src={ico_download} className="mr-2"></img><strong><h1 style={{fontSize:'20px'}}> English (Original)</h1></strong></button>
     </div>
   )
 }
--- src/pages/Whitepaper.jsx
+++ src/pages/Whitepaper.jsx
@@ -4,30 +4,33 @@ import { motion, useScroll, useSpring } from "framer-motion";
 import Navbar from "@/pagecomponents/shared/Navbar";
 import { useEffect } from "react";
 import Footer from "@/pagecomponents/shared/Footer";
-import WhitepaperIMG from "@/pagecomponents/whitepapercomponents/WhitepaperIMG/WhitepaperIMG";
 
 const Whitepaper = () => {
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
-  });
+  
+  
+  // const { scrollYProgress } = useScroll();
+  // const scaleX = useSpring(scrollYProgress, {
+  //   stiffness: 100,
+  //   damping: 30,
+  //   restDelta: 0.001,
+  // });
+
+
   useEffect(() => {
     document.title = "White paper | williamouyang.org";
   }, []);
 
   return (
-    <>
+    <div style={{display:'flex', height:'100vh', width:'100vw', flexDirection:'column'}}>
       <Navbar/>
-      <>
+      {/* <>
         <motion.div className="progress-bar" style={{ scaleX }} />
-        {/* <WhitepaperText />   */}
-        <WhitepaperIMG/>
-      </>
+      </> */}
+      <div style={{display:'flex', height:'100%'}}>
       <Download />
+      </div>
       <Footer/>
-    </>
+    </div>
   );
 };
 
